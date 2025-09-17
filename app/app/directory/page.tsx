fe29f806b9550e43a14fbb1ef5d7571dea5a81d4
llantrisantlocal <llantrisantlocal@gmail.com>
2025-09-18 00:00:33 +0100

    Create page.tsx

---
+++ app/app/directory/page.tsx
@@ -0,0 +1,8 @@
+export default function DirectoryPage() {
+  return (
+    <section style={{ padding: "20px" }}>
+      <h1>Directory</h1>
+      <p>This is the directory page. A list of local businesses will go here.</p>
+    </section>
+  );
+}
