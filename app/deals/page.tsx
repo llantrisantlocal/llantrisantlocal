1b308599bead583a2f121bc515ff6a1bb8847217
llantrisantlocal <llantrisantlocal@gmail.com>
2025-09-18 00:24:31 +0100

    Create page.tsx

---
+++ app/deals/page.tsx
@@ -0,0 +1,13 @@
+export default function Deals() {
+  return (
+    <section style={{ padding: "2rem" }}>
+      <h1>Local Deals</h1>
+      <p>Check out the best offers and discounts from Llantrisant businesses:</p>
+      <ul>
+        <li>10% off cleaning services this month</li>
+        <li>Free consultation for gardening projects</li>
+        <li>Discounted trades call-out fees</li>
+      </ul>
+    </section>
+  );
+}
